5844ab3e9fd143558ea51f2c96344f92ff6d1364
simong <simong1@kkszki.hu>
2025-09-17 12:19:56 +0200

    feladat 1 done

--- oopAlapok/Program.cs
+++ oopAlapok/Program.cs
@@ -6,12 +6,19 @@ using System.Threading.Tasks;
 
 namespace oopAlapok
 {
+    public class Szemely
+    {
+        public string _nev = "Nagy Jozsef";
+        public int _eletkor = 90;
+
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Szemely tanulo1 = new Szemely();
+            Console.WriteLine(tanulo1._nev);
+            Console.WriteLine(tanulo1._eletkor);
         }
     }
 }
